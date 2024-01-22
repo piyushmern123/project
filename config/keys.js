b37333163a8f9ba0a4d2248e9d11d6575f60c6b5
piyushmern123 <448piyush@gmail.com>
2024-01-22 06:51:02 +0530

    my first commit

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+    mongoURI: "YOUR MONGO URI",
+    secretOrKey: "secret",
+    googleClientID: "YOUR CLIENT ID,
+  googleClientSecret: "YOUR CLIENT SECRET",
+  Cookie:"YOUR COOKIE SECRET"
+  };
+  
